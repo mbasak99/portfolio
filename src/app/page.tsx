43856e87bfe43a9732e6b83b97e6d2119704087f
Monark Basak <monarkb01@gmail.com>
2024-08-06 09:29:40 -0400

    Removed random text I accidentally added in footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,7 @@ export default function Home() {
   return (
     <div className="font-sans home-container">
       <main>Hello World</main>
-      <footer>
-        this is pretty cool!
-        </footer>
+      <footer></footer>
     </div>
   );
 }
